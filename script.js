e1716f3f35fb00279f963f6a6b7343077f94d57a
Panna Szűcs <szucspann@gmail.com>
2022-11-04 16:42:58 +0100

    script

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let removeSong = () => {
-let tableRow = document.querySelector(".row")
-tableRow.remove();
+const RemoveRowButtons = document.querySelectorAll(
+    '[data-action="remove-row"]'
+);
+
+for (const button of RemoveRowButtons) {
+    button.addEventListener("click", function (e) {
+        const target = e.currentTarget;
+        const col = target.parentNode;
+        const row = col.parentNode;
+
+        row.remove();
+    });
 }
